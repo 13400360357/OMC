ccafd10f9cfaf0d468f67a423210f4a9062af4a4
13400360357 <321385585@qq.com>
2016-10-26 18:04:17 +0800

    version1

--- Test.py
+++ Test.py
@@ -1,7 +1,12 @@
 #coding:utf-8
 '''
 Created on 2016年10月26日
-
 @author: MengLei
 '''
-print'haha'
+from selenium import webdriver
+import time
+
+b=webdriver.Firefox()
+b.get('http://192.168.9.70:9999/WebRoot/')
+time.sleep(0.5)
+print b.find_element_by_xpath('/html/body').text
