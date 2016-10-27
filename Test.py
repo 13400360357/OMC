48e848b049ecb6553c1dc021371c2514675a0ea8
13400360357 <321385585@qq.com>
2016-10-27 11:33:18 +0800

    hellotest

--- Test.py
+++ Test.py
@@ -6,7 +6,9 @@ Created on 2016年10月26日
 from selenium import webdriver
 import time
 
-b=webdriver.Firefox()
-b.get('http://192.168.9.70:9999/WebRoot/')
-time.sleep(0.5)
-print b.find_element_by_xpath('/html/body').text
+# b=webdriver.Firefox()
+# b.get('http://192.168.9.70:9999/WebRoot/')
+# time.sleep(0.5)
+# print b.find_element_by_xpath('/html/body').text
+
+print 'hello'
