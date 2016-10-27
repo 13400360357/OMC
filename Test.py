232e60d3e1bd42e14f99a77a04db300ea609aa7b
13400360357 <321385585@qq.com>
2016-10-27 12:19:05 +0800

    add open omc

--- Test.py
+++ Test.py
@@ -5,15 +5,15 @@
 from selenium import webdriver
 import time
 
-# b=webdriver.Firefox()
-# b.get('http://192.168.9.70:9999/WebRoot/')
-# time.sleep(0.5)
-# print b.find_element_by_xpath('/html/body').text
-
-print 'hello'
 b=webdriver.Firefox()
-print '1'
-b.get('http://192.168.9.32:8080/smallcell/')
-print '2'
-b.maximize_window()
-print '3'
+b.get('http://192.168.9.70:9999/WebRoot/')
+time.sleep(0.5)
+print b.find_element_by_xpath('/html/body').text
+
+# print 'hello'
+# b=webdriver.Firefox()
+# print '1'
+# b.get('http://192.168.9.32:8080/smallcell/')
+# print '2'
+# b.maximize_window()
+# print '3'
