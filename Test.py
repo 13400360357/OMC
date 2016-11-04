b9d38a05b0a4f14fe79e243b1f4a1443b2e40252
13400360357 <321385585@qq.com>
2016-11-04 15:22:10 +0800

    coding:2

--- Test.py
+++ Test.py
@@ -13,7 +13,7 @@ time.sleep(0.5)
 # print b.find_element_by_xpath('/html/body').text
 print 'haha'
 print '哈哈'
-print u'生成Excel文件为： '+os.getcwd()+os.sep+'report'+os.sep+'LST_RESULT-'+datetime.now().date().isoformat()+'.xlsx'
+print '生成Excel文件为： '+os.getcwd()+os.sep+'report'+os.sep+'LST_RESULT-'+datetime.now().date().isoformat()+'.xlsx'
 print 'lala'
 # print 'hello'
 # b=webdriver.Firefox()
