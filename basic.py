6b9110e390f232f2aaf7945333066a000a50bd01
13400360357 <321385585@qq.com>
2017-01-16 10:47:47 +0800

    xiugai

--- basic.py
+++ basic.py
@@ -150,11 +150,11 @@ class CreateTxt:
     #===========================================================================
 class browser:
     def run(self):
-        self.b = webdriver.Chrome()
+#         self.b = webdriver.Chrome()
 #         self.b=webdriver.PhantomJS(executable_path='/home/meng/Downloads/env/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-#         self.b=webdriver.PhantomJS(executable_path=r'D:\phantomjs\bin\phantomjs.exe')
+        self.b=webdriver.PhantomJS(executable_path=r'D:\phantomjs\bin\phantomjs.exe')
         
-#         self.b.set_window_size(1920, 1080)
+        self.b.set_window_size(1920, 1080)
         time.sleep(2)
         self.b.maximize_window()
         return self.b
